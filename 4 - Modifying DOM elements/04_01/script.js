1191fbc3f93b3d4da455f9ec66f93f64f2c2dfd5
PLFLORCZUKT <Tomasz.Florczuk@tetrapak.com>
2025-03-05 13:17:46 +0100

    Done the exercise

--- 4 - Modifying DOM elements/04_01/script.js
+++ 4 - Modifying DOM elements/04_01/script.js
@@ -1,3 +1,7 @@
 function updatePage() {
-    
+    let messageDiv = document.getElementById("message");
+    messageDiv.innerText = "Hello world!";
+
+    let contentDiv = document.getElementById("content");
+    contentDiv.innerHTML = "<p>Updated content</p>";
 }
