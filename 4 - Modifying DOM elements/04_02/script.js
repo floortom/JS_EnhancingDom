9552476e99e7a61009499d6e795a6a801bfbc5b6
PLFLORCZUKT <Tomasz.Florczuk@tetrapak.com>
2025-03-05 15:03:02 +0100

    Done the exercises

--- 4 - Modifying DOM elements/04_02/script.js
+++ 4 - Modifying DOM elements/04_02/script.js
@@ -1,7 +1,24 @@
 function updatePage() {
     
     // Reading and changing the value of a text input
-    let nameInput = document.getElementById('nameInput');
-    console.log('Name:', nameInput.value);
-    nameInput.value = 'John Doe';
-}
+    let nameInput = document.getElementById("nameInput");
+    console.log("Name: ", nameInput.value);
+    nameInput.value = "Alfred";
+
+    // Dropdown input
+    let deptInput = document.getElementById("departmentSelect");
+    deptInput.value = "development";
+
+    // Radio buttons
+    let radioButtons = document.querySelectorAll("input[type='radio'][name='status']");
+    radioButtons.forEach(btn => {
+        if (btn.checked) {
+            console.log("Employee status: ", btn.value);
+        };
+    });
+
+    // Checkboxes
+    let subscribe = document.getElementById('newsletterCheckbox');
+    console.log("Newsletter subscription: ", subscribe.checked);
+    subscribe.checked = true;
+};
