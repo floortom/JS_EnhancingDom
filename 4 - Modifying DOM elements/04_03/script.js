ba9f54789c533502f1e4bca3304aa17df5231211
PLFLORCZUKT <Tomasz.Florczuk@tetrapak.com>
2025-03-05 19:19:49 +0100

    Completed exercises

--- 4 - Modifying DOM elements/04_03/script.js
+++ 4 - Modifying DOM elements/04_03/script.js
@@ -1,5 +1,10 @@
 function changeAttributes() {
     // Changing the source of an image
-    let exampleImage = document.getElementById('exampleImage');
-    exampleImage.setAttribute('src', 'puppy2.jpg');
-}
+    let image = document.getElementById("exampleImage");
+    image.setAttribute("src", "puppy2.jpg");
+
+    // change link
+    let link = document.getElementById("exampleLink");
+    link.setAttribute("href", "https://example2.com");
+    link.innerText = "Visit Example2.com";
+};
