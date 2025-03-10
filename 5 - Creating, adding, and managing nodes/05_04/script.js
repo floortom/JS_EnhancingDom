acf870cdd8049e0d927c846d8a5e66fd3b8e735c
PLFLORCZUKT <Tomasz.Florczuk@tetrapak.com>
2025-03-10 13:30:55 +0100

    Done the exercise

--- 5 - Creating, adding, and managing nodes/05_04/script.js
+++ 5 - Creating, adding, and managing nodes/05_04/script.js
@@ -1,5 +1,20 @@
 // Clone a list item and add it to a list
-let taskList = document.getElementById('taskList');
-let firstTask = taskList.firstElementChild.cloneNode(true);
-firstTask.textContent = 'Prepare quarterly report';
-taskList.appendChild(firstTask);
+let taskLst = document.getElementById("taskList");
+let frstTsk = taskLst.firstElementChild.cloneNode(true);
+frstTsk.textContent = "Review project";
+taskLst.insertBefore(frstTsk, taskLst.firstChild);
+
+// Replace an element
+let announce = document.getElementById("announcements");
+let newAnnouce = document.createElement("p");
+newAnnouce.innerText = "New office opening next week";
+let oldAnnounce = document.getElementById("announcementText");
+announce.replaceChild(newAnnouce, oldAnnounce);
+
+// Remove an element
+let removeButton = document.getElementById("removeButton");
+removeButton.addEventListener("click", () => {
+  if (announce.firstChild) {
+    announce.removeChild(announce.firstChild);
+  }
+});
