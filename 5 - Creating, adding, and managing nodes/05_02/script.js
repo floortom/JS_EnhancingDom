b1b7985eb3f4ec2b4ec4d00109a9232f89380428
PLFLORCZUKT <Tomasz.Florczuk@tetrapak.com>
2025-03-10 12:18:21 +0100

    done exercise

--- 5 - Creating, adding, and managing nodes/05_02/script.js
+++ 5 - Creating, adding, and managing nodes/05_02/script.js
@@ -1,6 +1,13 @@
 // Populating department dropdown
-let departmentSelect = document.getElementById('departmentSelect');
-['Sales', 'Marketing', 'Development'].forEach(department => {
-    let option = new Option(department, department.toLowerCase());
-    departmentSelect.add(option);
+let departmentSelect = document.getElementById("departmentSelect");
+["Sales", "Development", "HR", "Management"].forEach((dept) => {
+  let option = new Option(dept, dept.toLowerCase());
+  departmentSelect.add(option);
 });
+
+// Add profile picture
+let picDiv = document.getElementById("employeeProfile");
+let img = new Image();
+img.src = "Harris.jpg";
+img.alt = "Employee profile image";
+picDiv.appendChild(img);
