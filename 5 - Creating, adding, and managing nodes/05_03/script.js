340e58acb4f603be710ab57a882df8ac8f7248ef
PLFLORCZUKT <Tomasz.Florczuk@tetrapak.com>
2025-03-10 12:53:55 +0100

    Done the exercise

--- 5 - Creating, adding, and managing nodes/05_03/script.js
+++ 5 - Creating, adding, and managing nodes/05_03/script.js
@@ -1,5 +1,14 @@
 // AppendChild Demo
-let taskList = document.getElementById('taskList');
-let newTask = document.createElement('li');
-newTask.textContent = 'Organize team meeting';
-taskList.appendChild(newTask);
+let taskLst = document.getElementById("taskList");
+let newTask = document.createElement("li");
+newTask.textContent = "Organize team meeting";
+taskLst.appendChild(newTask);
+
+// Insert before
+let urgentTask = document.createElement("li");
+urgentTask.textContent = "Create project";
+taskLst.insertBefore(urgentTask, taskLst.firstChild);
+
+// InnerHTML
+let dynCont = document.getElementById("dynamicContent");
+dynCont.innerHTML = "<p>New project coming soon!</p>";
