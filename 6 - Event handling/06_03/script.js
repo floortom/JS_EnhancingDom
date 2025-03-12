e813aaf4bc61459f123cde074876cd6246f541de
PLFLORCZUKT <Tomasz.Florczuk@tetrapak.com>
2025-03-12 11:23:17 +0100

    done the exercise

--- 6 - Event handling/06_03/script.js
+++ 6 - Event handling/06_03/script.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let actionButton = document.getElementById('actionButton');
+document.addEventListener("DOMContentLoaded", () => {
+  let actionButton = document.getElementById("actionButton");
 
-    // Add the event listener
-    actionButton.addEventListener('click', handleActionOnce);
+  // Add the event listener
+  actionButton.addEventListener("click", handleActionOnce);
 });
 
 function handleActionOnce() {
-    let actionButton = document.getElementById('actionButton');
-    console.log('Action performed!');
-    // Remove the event listener after first execution
-    actionButton.removeEventListener('click', handleActionOnce);
+  let actionButton = document.getElementById("actionButton");
+  console.log("Action performed!");
+  // Remove event listener after first execution
+  actionButton.removeEventListener("click", handleActionOnce);
 }
