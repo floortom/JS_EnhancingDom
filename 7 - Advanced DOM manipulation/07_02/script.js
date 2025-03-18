9956bb8c95159250693ee83124ec53565e382cb0
PLFLORCZUKT <Tomasz.Florczuk@tetrapak.com>
2025-03-18 09:04:47 +0100

    Done the exercise

--- 7 - Advanced DOM manipulation/07_02/script.js
+++ 7 - Advanced DOM manipulation/07_02/script.js
@@ -1,7 +1,37 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const todoList = document.getElementById('todoList');
-    const newTaskInput = document.getElementById('newTaskInput');
-    const addTaskButton = document.getElementById('addTaskButton');
-    const removeFirstTaskButton = document.getElementById('removeFirstTaskButton');
-    const prioritizeLastTaskButton = document.getElementById('prioritizeLastTaskButton');
+document.addEventListener("DOMContentLoaded", () => {
+  const todoList = document.getElementById("todoList");
+  const newTaskInput = document.getElementById("newTaskInput");
+  const addTaskButton = document.getElementById("addTaskButton");
+  const removeFirstTaskButton = document.getElementById(
+    "removeFirstTaskButton"
+  );
+  const prioritizeLastTaskButton = document.getElementById(
+    "prioritizeLastTaskButton"
+  );
+
+  // add new task
+  addTaskButton.addEventListener("click", () => {
+    task = newTaskInput.value;
+    if (task) {
+      let li = document.createElement("li");
+      li.textContent = task;
+      todoList.appendChild(li);
+      newTaskInput.value = "";
+    }
+  });
+
+  // Remove the first task
+  removeFirstTaskButton.addEventListener("click", () => {
+    if (todoList.children.length > 0) {
+      todoList.removeChild(todoList.children[0]);
+    }
+  });
+
+  // Prioritize last task
+  prioritizeLastTaskButton.addEventListener("click", () => {
+    if (todoList.children.length > 1) {
+      let lastTask = todoList.lastElementChild;
+      todoList.insertBefore(lastTask, todoList.firstElementChild);
+    }
+  });
 });
